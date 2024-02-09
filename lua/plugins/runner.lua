b4975d4ebdea87967cc561b08374cbe306dd891b
archThur <arthur.prager.ea@gmail.com>
2024-02-09 10:42:47 -0300

    added a runner

---
+++ lua/plugins/runner.lua
@@ -0,0 +1,17 @@
+return {
+	{
+		"CRAG666/code_runner.nvim",
+		config = function()
+			require("code_runner").setup({
+				filetype = {
+					python = "python3 -u",
+					rust = {
+						"cd $dir &&",
+						"rustc $fileName &&",
+						"$dir/$fileNameWithoutExt",
+					},
+				},
+			})
+		end,
+	},
+}
